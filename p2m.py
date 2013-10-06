75a6c1a7a0a6cdb6592ab82392ecfb7acc5dd504
zblack <al@Albertos-Mac-Pro.local>
2013-10-06 19:16:39 +0200

    mostly rewritten, implemented keyboard splitting, added license

--- p2m.py
+++ p2m.py
@@ -1,102 +1,118 @@
-#!/usr/bin/python2.6
-
-#Polyphonic to Multitrack midi file Converter
-#Splits all the voices of a midi file into several mono tracks and writes the result in the output midi file
-#It will produce as many tracks as the maximum polyphony used at any given time in the whole file
-#no limits on the voices, you can sprawl on the keyboard :)
-#usage: python p2m.py -i inputfile
+#usage: python p2m.py -i inputfile -s splitpoint
 
 import os
 import sys
 import optparse
 from midifile import *
 
-class Voices(object):
-    def __init__(self):
-        #list of allocated voices/tracks, contains note numbers, 0 means a now free but previously played voice
-        self.voices = list()
+class ZonedTrack(Track):
+    # note = -1 => currently inactive
+    def __init__(self, zone, note):
+        Track.__init__(self)
+        self.zone = zone
+        self.note = note
+        #for stats and debug
+        self.minnote = 128
+        self.maxnote = 0
 
-    def SlotOf(self, note):
-        return self.voices.index(note)
+    def AddEvent(self, time, event):
+        self._events.append((time, event))
+        if isinstance(event, NoteOnEvent):
+            self.minnote = min(event.note, self.minnote)
+            self.maxnote = max(event.note, self.maxnote)
+    
+class ZonedWriter(Writer):
+    def __init__(self, split_note):
+        Writer.__init__(self)
+        #  0=C-1 up 127=G9
+        i = 2 if split_note[1:2] == '#' else 1
+        note = split_note[0:i].upper()
+        octave = int(split_note[i:i+1])
+        self.split = (octave + 1) * 12 + ['C','C#','D','D#','E','F','F#','G','G#','A','A#','B'].index(note)
+
+    def ZoneOf(self, note):
+        return "A" if note < self.split else "B"
+    
+    def AddTrack(self, note):
+        new_track = ZonedTrack(self.ZoneOf(note), note)
+        self._tracks.append(new_track)
+        return new_track
+
+    def FindFreeTrack(self, note):
+        zone = self.ZoneOf(note)
+        return next((t for t in self._tracks if t.zone == zone and t.note == -1), None)
+    
+    def TrackByNote(self, note):
+        return next((t for t in self._tracks if t.note == note), None)
     
     #looks for an empty slot, if not found allocates a new track
-    #returns the slot number
-    def NoteOn(self, note, velocity, w):
-        if velocity == 0:
-            NoteOff(note)
-        try:
-            i = self.SlotOf(0)
-        except:
-            self.voices.append(note)
-            i = len(self.voices) - 1
-            if i > 0:
-                w.AddTrack()
+    def NoteOn(self, d, e):
+        if e.velocity == 0:
+            self.NoteOff(d, e)
+            return
         
-        self.voices[i] = note
-        return i
+        t = self.FindFreeTrack(e.note)
+        if t is None:
+            t = self.AddTrack(e.note)
     
-    def NoteOff(self, note):
-        try:
-            i = self.SlotOf(note)
-            self.voices[i] = 0
-            return i
-        #it should never happen, but just in case it won't wreak havoc
-        except:
-            return -1
-    
-class Converter(object):
-    def __init__(self):
-        self.v = Voices()
-    
-    def Convert(self, ifname, ofname):
-        r = Reader()
-        w = Writer()
+        t.note = e.note
+        t.AddEvent(d, e) 
 
-        #a first track always needed for a no note event occurring before a note event
-        w.AddTrack()
-        
+    def NoteOff(self, d, e):
+        t = self.TrackByNote(e.note)
+        if not t is None:
+            t.note = -1
+            t.AddEvent(d, e) 
+            
+class Converter(object):
+
+    #identify a playable track
+    def PlayableTrack(self, t):
+        for _, e in t:
+            if isinstance(e, NoteOnEvent):
+                return True                 
+    
+        return False
+    
+    def Convert(self, ifname, ofname, split_note):
+        r = Reader()
+        w = ZonedWriter(split_note)
+
         fin = file(ifname, 'rb')
         r.Read(fin)
 
-        trackname='Track'
+        w.AddTrack(-1)
+        trackname=''
 
-        #Considering the limited scope of this script, tempo info tracks will be discarded
+        # given the limited scope of this script, tempo info tracks will be discarded
         for t in r.tracks:
-            #identify a playable track
-            playabletrack = False
-            for _, e in t:
-                if isinstance(e, NoteOnEvent):
-                    playabletrack = True                 
-                    break
-     
-            if playabletrack:     
+            if self.PlayableTrack(t):     
                 for d, e in t:
                     #voice specific events
                     if isinstance(e, ChannelEvent):
                         if isinstance(e, NoteOnEvent):
-                            i = self.v.NoteOn(e.note, e.velocity, w)
-                            #a bit unfair, but had not choice
-                            w._tracks[i].AddEvent(d, e) 
+                            w.NoteOn(d, e)
                         elif isinstance(e, NoteOffEvent):
-                            i = self.v.NoteOff(e.note)
-                            if i >= 0:
-                                w._tracks[i].AddEvent(d, e) 
+                            w.NoteOff(d, e)
                         elif isinstance(e, KeyAftertouchEvent):
-                            i = self.v.SlotOf(e.note)
-                            w._tracks[i].AddEvent(d, e) 
-                        #any else channel event must be copied as is and propagated to the other tracks
+                            w.TrackByNote(e.note).AddEvent(d, e) 
+                        #any other channel event must be copied as is and propagated to the other tracks
                         else:
-                            for i in range(0, len(w._tracks) - 1):
-                                w._tracks[i].AddEvent(d, e)
-                    #everything else except track names will be copied to the first track
+                            for t in w._tracks:
+                                t.AddEvent(d, e)
+                    #anything else except track names will be copied to the first track
                     else:
                         if isinstance(e, TrackNameEvent):
                             trackname = e.text
                         else:
                             w._tracks[0].AddEvent(d, e) 
         
+        #stick the tracks of the same group together
+        w._tracks.sort(None, lambda t: t.zone)
         for t in w._tracks:
-            t.AddEvent(1, TrackNameEvent(trackname + '_' + str(w._tracks.index(t) + 1)))
+            name = trackname + '_' + t.zone + str(w._tracks.index(t) + 1)
+            t.AddEvent(1, TrackNameEvent(name))
+            print name, t.minnote, t.maxnote
             
         w._ppq = r.ppq
         fout = file(ofname, 'wb')    
@@ -115,12 +131,19 @@ if __name__ == '__main__':
       default='Untitled.mid',
       help='input file FILE',
       metavar='FILE')
+    parser.add_option(
+      '-s',
+      '--split_note',
+      dest='split_note',
+      default='G#9',
+      help='split note letter/number format: C-1..G9',
+      metavar='SPLIT')
 
     options, _ = parser.parse_args()
 
     c = Converter()
     ifname = options.input_file 
     ofname = os.path.splitext(ifname)[0] + '_multi.mid'
-    i = c.Convert(ifname, ofname)
+    i = c.Convert(ifname, ofname, options.split_note)
 
     sys.stdout.write(str(i) + ' Mono Tracks Generated into ' + ofname)
